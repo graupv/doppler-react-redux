b7ce387023b39f2a530a00ae3c5d7caf955b8b51
graupv <38445832+graupv@users.noreply.github.com>
2020-05-17 19:17:01 -0600

    auth reducer y component

---
+++ doppler/src/components/LoginForm/index.js
@@ -0,0 +1,3 @@
+import { v4 as uuidv4 } from 'uuid';
+import React, { useState, Fragment } from 'react';
+import { connect } from 'react-redux';
---
+++ doppler/src/components/app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { configureStore } from '../../store';
+
+const { store, persistor } = configureStore();
+
+const App = () => (
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+
+        </PersistGate>
+    </Provider>
+);
+
+export default App;
