f03893cc2ccd8d8f6f7adaccc0f4defaee848c7a
graupv <38445832+graupv@users.noreply.github.com>
2020-05-25 01:13:06 -0600

    navbar with fragments and router link

--- doppler/src/components/navbar/navbar.js
+++ doppler/src/components/navbar/navbar.js
@@ -1,3 +1,37 @@
 import React, { Fragment } from 'react';
+import {Link} from 'react-router-dom';
 
 import './styles.css';
+
+const NavBar = ({
+
+}) => (
+    <div classname="NavBar">
+        <Fragment>
+            <span>
+                <Link to="/tracks">
+                    {/* icon or something */}
+                </Link>
+            </span>
+            <span>
+                <Link to="/phrasebox">
+                    {/* icon or something */}
+                </Link>
+            </span>
+            <span>
+                <Link to="/chordbox">
+                    {/* icon or something */}
+                </Link>
+            </span>
+            <span>
+                <Link to="/sharing">
+                    {/* icon or something */}
+                </Link>
+            </span>
+            <span>
+                {/* LOGOUT */}
+                
+            </span>
+        </Fragment>
+    </div>
+);
